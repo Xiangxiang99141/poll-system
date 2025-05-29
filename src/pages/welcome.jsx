d676e36d482aa830cf7247425bd665c09a64ba49
DogBrother <roys99141@gmail.com>
2025-05-29 23:23:08 +0800

    update welcome page

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
+import '../styles/welcome.css'
 
 export default function Welcome(){
     const [count, setCount] = useState(0)
     return(
-        <>
+        <div id="welcome">
             <div>
                 <a href="https://vite.dev" target="_blank">
                 <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -26,6 +27,6 @@ export default function Welcome(){
             <p className="read-the-docs">
                 Click on the Vite and React logos to learn more
             </p>
-        </>
+        </div>
     )
 }
