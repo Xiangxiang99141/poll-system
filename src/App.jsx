2b7fa220c9b3825899ba382d2b653e11b27b1714
DogBrother <roys99141@gmail.com>
2025-05-30 18:51:22 +0800

    新增 vote 頁面路由

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Manage from './pages/manage'
 import Welcome from './pages/welcome';
 import Login from './pages/login';
 import NotFound from './pages/404notfound';
+import Vote from './pages/vote';
+
 function App() {
   const isAdmin = localStorage.getItem('isAdmin') || false;
   return (
@@ -17,6 +19,7 @@ function App() {
           <Route path='/' element={<Welcome/>}></Route>
           <Route path='/manage' element={<Manage isAdmin={isAdmin}/>}></Route>
           <Route path='/login' element={<Login/>}></Route>
+          <Route path='/vote' element={<Vote/>}></Route>
           <Route path='*' element={<NotFound/>}></Route>
         </Routes>
       </Container>
