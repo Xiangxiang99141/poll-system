9be9f1fbfb8e7c1920617611015d62dd8350c099
DogBrother <roys99141@gmail.com>
2025-05-30 20:01:28 +0800

    更新 候選人資訊 component

--- src/components/CandidatesProfile.jsx
+++ src/components/CandidatesProfile.jsx
@@ -1,13 +1,31 @@
-import Card from 'react-bootstrap/Card';
-export default function CandidatesProfile({id, name, account=null, password=null,politics}){
+import {Card,Image,Badge} from 'react-bootstrap';
+export default function CandidatesProfile({id=null, name, account=null, password=null,politics,imageUrl='./default-photo.jpg',voteCount=0}){
     return(
         <>
             <Card>
-                <Card.Header>候選人資訊</Card.Header>
+                <Card.Header>
+                    候選人資訊
+                </Card.Header>
                 <Card.Body className='text-start'>
+                    <div className="d-flex justify-content-center">
+                        <Image
+                            src={imageUrl}
+                            roundedCircle
+                            style={{ width: "200px", height: "200px", objectFit: "cover"}}
+                            alt={name}
+                            className='mb-3'
+                        />
+                    </div>
                     <Card.Title>姓名：{name}</Card.Title>
                     <Card.Text>
-                        帳號Id：{id}<br/>
+                        {
+                            id && (
+                                    <>
+                                        帳號Id：{id}<br/>
+                                    </>
+                                )
+                        }
+                        
                         {account && (
                             <>
                                 帳號：{account}<br />
@@ -15,9 +33,10 @@ export default function CandidatesProfile({id, name, account=null, password=null
                         )}
                         {password && (
                             <>
-                                密碼：{password}
+                                密碼：{password}<br />
                             </>
                         )}
+                        票數：<Badge bg="primary">{voteCount}</Badge>
                     </Card.Text>
                     <Card.Footer>
                         政見：{politics}
