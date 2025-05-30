251f464f3de9e73024e996dc9f51196ce63de797
DogBrother <roys99141@gmail.com>
2025-05-30 20:01:58 +0800

    新增 顯示候選人票數

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -56,6 +56,7 @@ export default function Welcome(){
                                             id={candidate.id}
                                             name={candidate.name}
                                             politics={candidate.politics}
+                                            voteCount={candidate.votedCount}
                                         />
                                     </Col>
                                 )
