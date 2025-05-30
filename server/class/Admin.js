4a89fdba8039aca467dd5261ff023d876a80ffb6
DogBrother <roys99141@gmail.com>
2025-05-30 12:47:24 +0800

    新增 儲存候選人及選民

--- server/class/Admin.js
+++ server/class/Admin.js
@@ -16,17 +16,26 @@ class Admin extends User{
         }
         return voters
     }
-    save(voters,path="./data/voters.json"){
-        if(voters.length>0){
+    saveVoter(voters,path="./data/voters.json"){
+        try{
+            let json = JSON.stringify(voters,null,4);
+            fs.writeFileSync(path,json);
+            return {state:true,error:""}
+        }catch(error){
+            return {state:false,error:error}
+        }
+    }
+    saveCandidate(candidates,path="./data/candidates.json"){
+        if(candidates.length>0){
             try{
-                let json = JSON.stringify(voters,null,4);
+                let json = JSON.stringify(candidates,null,4);
                 fs.writeFileSync(path,json);
                 return {state:true,error:""}
             }catch(error){
                 return {state:false,error:error}
             }
         }else{
-            return {state:false,error:"尚未產生帳號"}
+            return {state:false,error:"尚未新增候選人"}
         }
     }
 }
